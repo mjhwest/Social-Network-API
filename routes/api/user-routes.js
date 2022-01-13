36193c6ca00aaf2c96585a45b90d9c3120058571
mjhwest89 <mjhwest89@gmail.com>
2022-01-13 14:10:47 +1030

    added server.js

---
+++ routes/api/user-routes.js
@@ -0,0 +1,9 @@
+//USER ROUTES NEEDED
+//1) GET ALL USERS, 
+//2) GET a SINGLE user by its _id and populated thought and friend data, 
+//3) POST a NEW user, 
+//4) PUT to UPDATE a USER by its _id, 
+//5) DELETE to REMOVE a users by its _id
+
+
+//3) POST a NEW user,
