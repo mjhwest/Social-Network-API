11bdaf1672db84696a13d147b9000afc71b0bd08
mjhwest89 <mjhwest89@gmail.com>
2022-01-13 14:46:09 +1030

    user routes added

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -6,4 +6,30 @@
 //5) DELETE to REMOVE a users by its _id
 
 
-//3) POST a NEW user,
+//3) POST (Create) a NEW user,
+
+const router = require('express').Router();
+const {
+    //POST
+    createUser,
+    addNewFriend,
+    //GET
+    getAllUsers,
+    getUserById,
+    //PUT
+    updateUserById,
+    //DELETE
+    deleteUserById,
+    removeFriend
+} = require('../../controllers/user-controller');
+
+//api/users ----------GET all Users and POST a NEW USER 
+router.route('/').get(getAllUsers).post(createUser);
+
+//api/users/:id -------GET USER BY ID, UPDATE USER BY ID and DELETE USER BY ID 
+router.route('/:id').get(getUserById).put(updateUserById).delete(deleteUserById);
+
+//api/users/:userId/friends/:friendId -----POST /NEW FRIEND TO USERS FRIEND LIST and REMOVE A FRIEND 
+router.route('/:id/friends/:friendsId').post(addNewFriend).delete(removeFriend);
+
+module.exports = router;
