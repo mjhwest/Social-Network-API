467bb3911006ebe9fcd097397fa71dcfb4af17f4
mjhwest89 <mjhwest89@gmail.com>
2022-01-13 18:02:51 +1030

    push to save

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -16,13 +16,10 @@ const {
 //api/users ----------GET all Users and POST a NEW USER 
 router.route('/').get(getAllUsers).post(createUser);
 
-//or
-// router.route('/').post(createUser)
-
 //api/users/:id -------GET USER BY ID, UPDATE USER BY ID and DELETE USER BY ID 
-router.route('/:id').get(getUserById).put(updateUserById).delete(deleteUserById);
+// router.route('/:id').get(getUserById).put(updateUserById).delete(deleteUserById);
 
 //api/users/:userId/friends/:friendId -----POST /NEW FRIEND TO USERS FRIEND LIST and REMOVE A FRIEND 
-router.route('/:id/friends/:friendsId').post(addNewFriend).delete(removeFriend);
+// router.route('/:id/friends/:friendsId').post(addNewFriend).delete(removeFriend);
 
 module.exports = router;
